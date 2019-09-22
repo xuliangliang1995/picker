8c658b420bfb15403ab165d86c84ec55c1d794f7
xuliangliang <835547206@qq.com>
2019-09-22 11:03:45 +0800

    添加 picker-user 工程

---
+++ program/picker-user/src/main/java/com/grasswort/picker/user/bootstrap/PickerUserApplication.java
@@ -0,0 +1,21 @@
+package com.grasswort.picker.user.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author xuliangliang
+ * @Classname PickerUserApplication
+ * @Description TODO
+ * @Date 2019/9/22 10:24
+ * @blame Java Team
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.grasswort.picker.user.controller"})
+public class PickerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickerUserApplication.class, args);
+    }
+}
---
+++ program/picker-user/src/main/java/com/grasswort/picker/user/controller/RegisterController.java
@@ -0,0 +1,38 @@
+package com.grasswort.picker.user.controller;
+
+import com.grasswort.picker.commons.result.ResponseData;
+import com.grasswort.picker.commons.result.ResponseUtil;
+import com.grasswort.picker.user.IUserRegisterService;
+import com.grasswort.picker.user.constants.SysRetCodeConstants;
+import com.grasswort.picker.user.dto.UserRegisterRequest;
+import com.grasswort.picker.user.dto.UserRegisterResponse;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xuliangliang
+ * @Classname RegisterController
+ * @Description 注册
+ * @Date 2019/9/22 10:37
+ * @blame Java Team
+ */
+@RestController
+@RequestMapping("/register")
+public class RegisterController {
+    @Reference(timeout = 3000)
+    private IUserRegisterService iUserRegisterService;
+
+    @PostMapping
+    public ResponseData register(@RequestBody UserRegisterRequest body) {
+        body.requestCheck();
+        UserRegisterResponse result = iUserRegisterService.register(body);
+        if (result.getCode().equals(SysRetCodeConstants.SUCCESS.getCode())) {
+            return new ResponseUtil<>().setData(null);
+        }
+        return new ResponseUtil<>().setErrorMsg(result.getMsg());
+    }
+
+}
