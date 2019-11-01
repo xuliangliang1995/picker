ad9a3d8f4972a779a95aa3206ba2114236501eb2
xuliangliang <835547206@qq.com>
2019-11-01 23:24:31 +0800

    博客列表添加摘要、配图、标签信息

---
+++ program/picker-service-blog/blog-service-provider/src/main/java/com/grasswort/picker/blog/dao/persistence/ext/BlogLabelDao.java
@@ -0,0 +1,21 @@
+package com.grasswort.picker.blog.dao.persistence.ext;
+
+import com.grasswort.picker.blog.dao.entity.BlogLabel;
+import com.grasswort.picker.commons.tkmapper.TkMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * @author xuliangliang
+ * @Classname BlogLabelDao
+ * @Description 博客标签
+ * @Date 2019/11/1 23:17
+ * @blame Java Team
+ */
+public interface BlogLabelDao extends TkMapper<BlogLabel> {
+
+    @Select("select label from pk_blog_label where blog_id = #{blogId}")
+    List<String> listBlogLabels(@Param("blogId") Long blogId);
+}
