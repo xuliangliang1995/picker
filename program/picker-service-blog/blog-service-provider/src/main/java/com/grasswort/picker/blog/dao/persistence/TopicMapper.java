71b739ec36e6dcfaa73d5c07251ad35d75226310
xuliangliang <835547206@qq.com>
2019-12-17 16:06:48 +0800

    修改用户信息修改后更新专题信息

--- program/picker-service-blog/blog-service-provider/src/main/java/com/grasswort/picker/blog/dao/persistence/TopicMapper.java
+++ program/picker-service-blog/blog-service-provider/src/main/java/com/grasswort/picker/blog/dao/persistence/TopicMapper.java
@@ -2,6 +2,13 @@ package com.grasswort.picker.blog.dao.persistence;
 
 import com.grasswort.picker.blog.dao.entity.Topic;
 import com.grasswort.picker.commons.tkmapper.TkMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface TopicMapper extends TkMapper<Topic> {
+
+    @Select("select id from pk_topic where pk_user_id = #{authorId}")
+    List<Long> listTopic(@Param("authorId") long authorId);
 }
--- program/picker-service-blog/blog-service-provider/src/main/java/com/grasswort/picker/blog/service/BlogRefreshService.java
+++ program/picker-service-blog/blog-service-provider/src/main/java/com/grasswort/picker/blog/service/BlogRefreshService.java
@@ -2,11 +2,15 @@ package com.grasswort.picker.blog.service;
 
 import com.grasswort.picker.blog.IBlogRefreshService;
 import com.grasswort.picker.blog.constant.DBGroup;
+import com.grasswort.picker.blog.dao.entity.Topic;
+import com.grasswort.picker.blog.dao.persistence.TopicMapper;
 import com.grasswort.picker.blog.dao.persistence.ext.BlogDao;
 import com.grasswort.picker.blog.service.elastic.BlogDocUpdateService;
+import com.grasswort.picker.blog.service.elastic.TopicDocRefreshService;
 import com.grasswort.picker.commons.annotation.DB;
 import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Example;
 
 /**
  * @author xuliangliang
@@ -21,6 +25,10 @@ public class BlogRefreshService implements IBlogRefreshService {
     @Autowired BlogDocUpdateService blogDocUpdateService;
 
     @Autowired BlogDao blogDao;
+
+    @Autowired TopicMapper topicMapper;
+
+    @Autowired TopicDocRefreshService topicDocRefreshService;
     /**
      * 刷新某位作者相关博客 ES 存储
      *
@@ -32,6 +40,11 @@ public class BlogRefreshService implements IBlogRefreshService {
         if (authorId != null && authorId > 0L) {
             blogDao.listBlogIdByAuthorId(authorId).parallelStream()
                     .forEach(blogId -> blogDocUpdateService.updateBlogDoc(blogId));
+
+            Example example = new Example(Topic.class);
+            example.createCriteria().andEqualTo("pkUserId", authorId);
+            topicMapper.selectByExample(example).stream()
+                    .forEach(topic -> topicDocRefreshService.refreshTopic(topic));
         }
     }
 }
